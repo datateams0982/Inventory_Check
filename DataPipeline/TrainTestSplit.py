869cd0ba7de6723d96bab49a1db18d998539241b
Chen, Yi-Wen <b01a01207@ntu.edu.tw>
2019-10-23 17:28:54 +0800

    1023

--- DataPipeline/TrainTestSplit.py
+++ DataPipeline/TrainTestSplit.py
@@ -8,6 +8,80 @@ import os
 from datetime import datetime, timedelta, date
 
 
-class TrainTestSplit:
+class TrainProcess:
 
-    def __init__(self, df_path, cluster_num, )
+    def __init__(self, df_path, cluster_num, train_date, val_date, 
+                    feature_list=['close', 'high', 'low', 'open',
+                    'vol', 'VWAP', 'ma7', 'ma21', '26ema', '12ema', 'MACD', 'upper_band',
+                    'lower_band', 'skew', 'kurtosis', 'pvt', 'ATR',
+                    'RSI_15', 'SO%k5', 'SO%d3']):
+
+        self._df = pd.read_csv(f'{df_path}Cluster_{cluster_num}.csv', converters={'ts': str, 'StockNo': str, 'StockName': str})
+        self._df['ts'] = pd.to_datetime(self._df['ts'])
+        self._traindate = train_date
+        self._valdate = val_date
+        self._train_df = self._df[self._df['ts'].dt.date < self._traindate][self._df['26ema'].notnull()]
+        self._val_df = self._df[self._df['ts'].dt.date < self._valdate = val_date][self._df['ts'].dt.date >= self._traindate][self._df['26ema'].notnull()]
+        self._test_df = self._df[self._df['ts'].dt.date >= self._valdate = val_date][self._df['26ema'].notnull()]
+        self._train_scaled = []
+        self._val_scaled = []
+        self._test_scaled = []
+        self._feature = feature_list
+
+        if (not self._train_df[self._feature].isna().values.any()) or (not self._val_df[self._feature].isna().values.any()) or (not self._test_df[self._feature].isna().values.any()):
+            raise ('Exist Missing Value in data')
+
+    
+    @property
+    def train_df(self):
+        return self._train_df
+
+    @property
+    def val_df(self):
+        return self._val_df
+
+    @property
+    def test_df(self):
+        return self._test_df
+
+    @property
+    def train_scaled(self):
+        return self._train_scaled
+
+    @property
+    def val_scaled(self):
+        return self._val_scaled
+
+    @property
+    def test_scaled(self):
+        return self._test_scaled
+
+    @property
+    def feature(self):
+        return self._feature
+
+
+    def _GetScalerParam(self):
+
+        self._paramdf = pd.DataFrame(columns=['max', 'min', 'std', 'mean'])
+        self._paramdf['max'] = self._train_df[self._feature].max()
+        self._paramdf['min'] = self._train_df[self._feature].min()
+
+
+    def _Scaler(self):
+
+        self._GetScalerParam()
+
+        Max = self._paramdf['max']
+        Min = self._paramdf['min']
+
+        for f in self._feature:
+            self._train_scaled[f] = (self._train_df[f] - Min.loc[f])/(Max.loc[f] - Min.loc[f])
+            self._val_scaled[f] = (self._val_df[f] - Min.loc[f])/(Max.loc[f] - Min.loc[f])
+            self._test_scaled[f] = (self._test_df[f] - Min.loc[f])/(Max.loc[f] - Min.loc[f])
+
+    
+
+
+
+
